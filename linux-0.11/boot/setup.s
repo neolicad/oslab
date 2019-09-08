ef3e574faec60fdf87b5d3a696266b0e06510a6e
neolicad <eredali@gmail.com>
2019-09-08 15:09:33 -0700

    read harware info finished, next need to print them

--- linux-0.11/boot/setup.s
+++ linux-0.11/boot/setup.s
@@ -1,3 +1,4 @@
+INITSEG = 0x7c0
 entry _start
 _start:
     mov ah,#0x03        
@@ -10,8 +11,28 @@ _start:
     mov es,ax
     mov ax,#0x1301   
     int 0x10
-inf_loop:
-    jmp inf_loop  
+    ! set ds to where bootsect resided
+    mov ax,#INITSEG
+    mov ds,ax
+    ! read cursor position
+    mov ah,#0x03
+    xor bh,bh
+    int 0x10
+    mov [0], dx
+    ! read memory info
+    mov ah,#0x88
+    int #0x15
+    mov [2],ax
+    ! read disk info
+    mov ax,#0x0000
+    mov ds,ax
+    lds si,[4 * 0x41]
+    mov ax,#INITSEG
+    mov es,ax
+    mov di,#0x0004
+    mov cx,#0x10
+    rep
+    movsb
 msg1:
     .byte   13,10                     
     .ascii  "Now we are in SETUP"
